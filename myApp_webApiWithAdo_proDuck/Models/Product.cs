156de790e9839123c0a85f0f4d71ed9e273ca37b
VisualStudio2022 <vs2022@mail.it>
2025-06-22 10:55:44 +0700

    add model product

---
+++ myApp_webApiWithAdo_proDuck/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace myApp_webApiWithAdo_proDuck.Models {
+  public class Product {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public long Price { get; set; }
+  }
+}
