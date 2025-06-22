f6e516b269f68d49d78ae36cd12de6f2d13f68a4
VisualStudio2022 <vs2022@mail.it>
2025-06-22 10:34:40 +0700

    Add project files.

---
+++ myApp_webApiWithAdo_proDuck/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace myApp_webApiWithAdo_proDuck{
+  public class Program{
+    public static void Main(string[] args){
+      /* ============================== services ============================== */
+      var builder = WebApplication.CreateBuilder(args);
+      builder.Services.AddControllers();
+      /* ============================== pipeline ============================== */
+      var app = builder.Build();
+      app.MapControllers();
+      app.Run();
+    }
+  }
+}
