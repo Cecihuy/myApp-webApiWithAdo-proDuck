6fa7d038db94e5d5d88dccdffe3180723f35e3e5
VisualStudio2022 <vs2022@mail.it>
2025-06-22 10:35:02 +0700

    initial commit

--- myApp_webApiWithAdo_proDuck/Program.cs
+++ myApp_webApiWithAdo_proDuck/Program.cs
@@ -4,9 +4,11 @@ using Microsoft.Extensions.DependencyInjection;
 namespace myApp_webApiWithAdo_proDuck{
   public class Program{
     public static void Main(string[] args){
+
       /* ============================== services ============================== */
       var builder = WebApplication.CreateBuilder(args);
       builder.Services.AddControllers();
+
       /* ============================== pipeline ============================== */
       var app = builder.Build();
       app.MapControllers();
